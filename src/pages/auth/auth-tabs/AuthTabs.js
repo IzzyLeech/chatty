32ccec85c372f109b6c64f1b5511917616403eed
IzzyLeech <ralphhunter46@gmail.com>
2025-10-17 17:55:43 +0100

    add backgroudimage to auth tabs

--- src/pages/auth/auth-tabs/AuthTabs.js
+++ src/pages/auth/auth-tabs/AuthTabs.js
@@ -1,27 +1,30 @@
-import './AuthTabs.scss'
+import { useState } from 'react';
+import './AuthTabs.scss';
+import backgroundImage from '../../../assets/images/background.jpg';
 
 const AuthTabs = () => {
+  const [type, setType] = useState('Sign In');
   return (
     <>
-    <div className="container-wrapper">
-      <div className="environment">DEV</div>
-      <div className="container-wrapper-auth">
-        <div className="tabs">
-          <div className="tabs-auth">
-            <ul className="tab-group">
-              <li className="tab active">
-                <button className="login">Sign In</button>
-              </li>
-              <li className="tab">
-                <button className="signup">Sign Up</button>
-              </li>
-            </ul>
-            <div className="tab-item">login component</div>
-            <div className="tab-item">register component</div>
+      <div className="container-wrapper" style={{backgroundImage: `url(${backgroundImage})` }}>
+        <div className="environment">DEV</div>
+        <div className="container-wrapper-auth">
+          <div className="tabs">
+            <div className="tabs-auth">
+              <ul className="tab-group">
+                <li className="tab active" on onClick={() => setType('Sign In')}>
+                  <button className="login">Sign In</button>
+                </li>
+                <li className="tab" on onClick={() => setType('Sign Up')}>
+                  <button className="signup">Sign Up</button>
+                </li>
+              </ul>
+              {type === 'Sign In' && (<div className="tab-item">login component</div>)}
+              {type === 'Sign Up' && (<div className="tab-item">register component</div>)}
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
