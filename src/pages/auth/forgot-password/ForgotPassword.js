97d1314e18ce75de7b705b5e176bc5c595e882aa
IzzyLeech <ralphhunter46@gmail.com>
2025-10-18 12:45:58 +0100

    created forgot passwoed and reset password cmponents

--- src/pages/auth/forgot-password/ForgotPassword.js
+++ src/pages/auth/forgot-password/ForgotPassword.js
@@ -4,42 +4,51 @@ import Input from '../../../components/input/Input';
 import Button from '../../../components/button/Button';
 import { FaArrowLeft } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import backgroundImage from '../../../assets/images/background.jpg';
 
 export const ForgotPassword = () => {
   return (
-    <div className="auth-inner">
-      {/* <div className="alerts" role="alert">
-        Error message
-      </div> */}
-      <form className="auth-form">
-        <div className="form-input-container">
-          <Input
-            id="username"
-            name="username"
-            type="text"
-            value="my value"
-            labelText="Username"
-            placeholder="Enter Username"
-            handleChange={() => {}}
-          />
-          <Input
-            id="password"
-            name="password"
-            type="password"
-            value="my value"
-            labelText="Password"
-            placeholder="Enter Password"
-            handleChange={() => {}}
-          />
-        </div>
-        <Button label={'SIGNIN'} className="auth-button button" disavled={true} />
+    <div className="container-wrapper" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="environment">DEV</div>
+      <div className="container-wrapper-auth">
+        <div className="tabs forgot-password-tabs">
+          <div className="tabs-auth">
+            <ul className="tab-group">
+              <li className="tab">
+                <div className="login forgot-password">Forgot Password</div>
+              </li>
+            </ul>
 
-        <Link to={'/'}>
-          <span className="forgot-password">
-            <FaArrowLeft className="arrow-left" /> back to Login
-          </span>
-        </Link>
-      </form>
+            <div className="tab-item">
+              <div className="auth-inner">
+                {/* <div className="alerts" role="alert">
+                    Error message
+                </div> */}
+                <form className="forgot-password-form">
+                  <div className="form-input-container">
+                    <Input
+                      id="email"
+                      name="email"
+                      type="text"
+                      value="tester@test.com"
+                      labelText="Email"
+                      placeholder="Enter Email"
+                      handleChange={() => {}}
+                    />
+                  </div>
+                  <Button label={'SIGNIN'} className="auth-button button" disavled={true} />
+
+                  <Link to={'/'}>
+                    <span className="login">
+                      <FaArrowLeft className="arrow-left" /> back to Login
+                    </span>
+                  </Link>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
