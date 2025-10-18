e24c2f3f1f8c061cc0b0b38a3308a5fe52545d13
IzzyLeech <ralphhunter46@gmail.com>
2025-10-18 13:42:26 +0100

    created auth service file with methods to receive post data

---
+++ src/services/axios.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const BASE_URL = `${process.env.REACT_APP_BASE_ENDPOINT}`;
+
+export default axios.create({
+  baseURL: BASE_URL,
+  headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+  withCredentials: true
+});
