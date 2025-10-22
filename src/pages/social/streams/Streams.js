3b6b4adc245630a7a4bc91d73973fc3b9306f80c
IzzyLeech <ralphhunter46@gmail.com>
2025-10-22 16:38:28 +0100

    built out the streams page component

--- src/pages/social/streams/Streams.js
+++ src/pages/social/streams/Streams.js
@@ -1,4 +1,22 @@
+import { useRef } from 'react';
+import '@pages/social/streams/Streams.scss';
+
 const Streams = () => {
-  return <div>Streams Component</div>;
+  const bodyRef = useRef(null);
+  const bottomLineRef = useRef();
+  return (
+    <div className="streams" data-testid="streams">
+      <div className="streams-content">
+        <div className="streams-post" ref={bodyRef} style={{ backgroundColor: 'white' }}>
+          <div>Post Form</div>
+          <div>Post Items</div>
+          <div ref={bottomLineRef} style={{ marginBottom: '50px', height: '50px' }}></div>
+        </div>
+        <div className="streams-suggestions">
+          <div>User Suggestions</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Streams;
