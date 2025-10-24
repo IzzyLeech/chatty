b33dc1e3bd400f2ad48467714ab0b15c896d891a
IzzyLeech <ralphhunter46@gmail.com>
2025-10-24 13:44:24 +0100

    created socketIO client

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import '@root/App.scss';
 import { AppRouter } from './routes';
+import { useEffect } from 'react';
+import { socketServive } from '@services/socket/socket.service';
 
 export const App = () => {
+  useEffect(() => {
+    socketServive.setupSocketConnection();
+  }, []);
+
   return (
     <>
       <BrowserRouter>
