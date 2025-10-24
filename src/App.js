882e796cb93fee9587f16e9b139bbf220c64a349
IzzyLeech <ralphhunter46@gmail.com>
2025-10-24 17:41:10 +0100

    added toast component to app component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,24 @@ import '@root/App.scss';
 import { AppRouter } from './routes';
 import { useEffect } from 'react';
 import { socketServive } from '@services/socket/socket.service';
+import Toast from '@components/toast/Toast';
+// import checkIcon from '@assets/images/check.svg';
+// import errorIcon from '@assets/images/error.svg';
+// import infoIcon from '@assets/images/info.svg';
+// import warningIcon from '@assets/images/warning.svg';
 
 export const App = () => {
+  const notifications = [];
+
   useEffect(() => {
     socketServive.setupSocketConnection();
   }, []);
 
   return (
     <>
+      {notifications && notifications.length > 0 && (
+        <Toast position="top-right" toastList={notifications} autoDelete={true} />
+      )}
       <BrowserRouter>
         <AppRouter />
       </BrowserRouter>
