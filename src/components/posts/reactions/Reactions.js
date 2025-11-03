55f081b64634621194bc7ed4a54e965e593d904d
IzzyLeech <ralphhunter46@gmail.com>
2025-11-03 13:22:12 +0000

    created rection component

---
+++ src/components/posts/reactions/Reactions.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '@components/posts/reactions/Reactions.scss';
+import { reactionsMap } from '@services/utils/static.data';
+
+const Reactions = ({ handleClick, showLabel = true }) => {
+  const reactionsList = ['like', 'love', 'wow', 'happy', 'sad', 'angry'];
+
+  return (
+    <div className="reactions" data-testid="reactions">
+      <ul>
+        {reactionsList.map((reaction, index) => (
+          <li key={index} onClick={() => handleClick(reaction)} data-testid="reaction">
+            {showLabel && <label>{reaction}</label>}
+            <img src={reactionsMap[reaction]} alt="" />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+Reactions.propTypes = {
+  handleClick: PropTypes.func,
+  showLabel: PropTypes.bool
+};
+
+export default Reactions;
