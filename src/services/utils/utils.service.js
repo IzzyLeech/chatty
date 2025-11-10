a21b1a9df111ace04cf73275f64a6e0727c3219a
IzzyLeech <ralphhunter46@gmail.com>
2025-11-10 14:48:13 +0000

    created follow utils service

--- src/services/utils/utils.service.js
+++ src/services/utils/utils.service.js
@@ -1,4 +1,4 @@
-import { floor, random, some } from 'lodash';
+import { findIndex, floor, random, some } from 'lodash';
 import { avatarColors } from '@services/utils/static.data';
 import { addUser, clearUser } from '@redux/reducers/user/user.reducer';
 import { addNotification, clearNotification } from '@redux/reducers/notifications/notification.reducer';
@@ -131,5 +131,11 @@ export class Utils {
 
   static getImage(imageId, imageVersion) {
     return imageId && imageVersion ? this.appImageUrl(imageVersion, imageId) : '';
+  }
+
+  static removeUserFromList(list, userId) {
+    const index = findIndex(list, (id) => id === userId);
+    list.splice(index, 1);
+    return list;
   }
 }
