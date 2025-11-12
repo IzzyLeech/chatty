fa38aa1814b54f48669c477ace495ee036130b13
IzzyLeech <ralphhunter46@gmail.com>
2025-11-12 13:05:16 +0000

    refactor map key index

--- src/components/suggestions/Suggestions.js
+++ src/components/suggestions/Suggestions.js
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 import '@components/suggestions/Suggestions.scss';
+import { Utils } from '@services/utils/utils.service';
 
 const Suggestions = () => {
   const [users, setUsers] = useState([]);
@@ -22,8 +23,8 @@ const Suggestions = () => {
       <hr />
       <div className="suggestions-container">
         <div className="suggestions">
-          {users?.map((user, index) => (
-            <div data-testid="suggestions-item" className="suggestions-item" key={index}>
+          {users?.map((user) => (
+            <div data-testid="suggestions-item" className="suggestions-item" key={Utils.generateString(10)}>
               <Avatar
                 name={user?.username}
                 bgColor={user?.avatarColor}
