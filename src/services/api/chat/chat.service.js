1b1c3058518dc82818206e4fb0a4cb96135a763d
IzzyLeech <ralphhunter46@gmail.com>
2025-11-19 17:06:49 +0000

    create chat utils file

--- src/services/api/chat/chat.service.js
+++ src/services/api/chat/chat.service.js
@@ -5,6 +5,11 @@ class ChatService {
     const response = await axios.get('/chat/message/conversation-list');
     return response;
   }
+
+  async removeChatUsers(body) {
+    const response = await axios.get('/chat/message/remove-chat-users', body);
+    return response;
+  }
 }
 
 export const chatService = new ChatService();
