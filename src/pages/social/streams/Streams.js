5ef160c414cc6b184c1da3a963120044fefe8159
IzzyLeech <ralphhunter46@gmail.com>
2025-11-26 13:53:43 +0000

    changed useSelector variablew to add reducer

--- src/pages/social/streams/Streams.js
+++ src/pages/social/streams/Streams.js
@@ -13,8 +13,8 @@ import { orderBy, uniqBy } from 'lodash';
 import useInfiniteScroll from '@hooks/useInfiniteScroll';
 import { PostUtils } from '@services/utils/post-utils-service';
 import useLocalStorage from '@hooks/useLocalStorage';
-import { addReactions } from '@redux/reducers/post/user-post-reaction.reducer';
 import { followerService } from '@services/api/followers/follower.service';
+import { useLocation } from 'react-router-dom';
 
 const Streams = () => {
   const [posts, setPosts] = useState([]);
@@ -25,7 +25,13 @@ const Streams = () => {
   const bottomLineRef = useRef();
   const dispatch = useDispatch();
   const storedUsername = useLocalStorage('username', 'get');
-  const { allPosts } = useSelector((state) => state);
+const location = useLocation();
+  const {
+    posts: reduxPosts,
+    totalPostsCount: reduxTotalPostsCount,
+    isLoading: reduxIsLoading
+  } = useSelector((state) => state.allPosts);
+
   let appPosts = useRef([]);
   const PAGE_SIZE = 10;
   const [currentPage, setCurrentPage] = useState(1);
@@ -83,16 +89,15 @@ const Streams = () => {
   });
 
   useEffect(() => {
-    setLoading(allPosts?.isLoading);
-    const orderedPosts = orderBy(allPosts?.posts, ['createdAt'], ['desc']);
+    setLoading(reduxIsLoading);
+    const orderedPosts = orderBy(reduxPosts, ['createdAt'], ['desc']);
     setPosts(orderedPosts);
-    setTotalPostsCount(allPosts?.totalPostsCount);
-  }, [allPosts]);
+    setTotalPostsCount(reduxTotalPostsCount);
+  }, [reduxPosts, reduxTotalPostsCount, reduxIsLoading]);
 
   useEffect(() => {
     PostUtils.socketIOPost(posts, setPosts);
   }, [posts]);
-
   return (
     <div className="streams" data-testid="streams">
       <div className="streams-content">
